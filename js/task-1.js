8e972de2e6d8d2e86769d302e82fe09fdacc1173
Artem Strelnikov <piramida1237@gmail.com>
2025-07-27 20:04:20 +0200

    ssdxcdfvfc

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,5 @@
 const categories = document.querySelector("#categories");
 const totalCategories = categories.children;
+console.log(`Number of categores: ${totalCategories.length}`);
 console.log(totalCategories);
+[...totalCategories];
