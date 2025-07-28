b69de6bbf3e387a70971f8771c11649129dc8398
Artem Strelnikov <piramida1237@gmail.com>
2025-07-28 14:43:58 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,8 @@
 const categories = document.querySelector("#categories");
 const totalCategories = categories.children;
+console.log(totalCategories);
 console.log(`Number of categores: ${totalCategories.length}`);
 [...totalCategories].forEach((element) => {
-  console.log(element.textContent);
+  console.log(`Category: ${element.firstElementChild.textContent}`);
+  console.log(`Elements: ${element.lastElementChild.children.length}`);
 });
