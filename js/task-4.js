a4a89dae7d126b05534d477b3b5be9a532e3b2f5
Artem Strelnikov <piramida1237@gmail.com>
2025-07-30 16:09:10 +0200

    add.prDef

--- js/task-4.js
+++ js/task-4.js
@@ -2,6 +2,7 @@ const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", hendleSubmit);
 
-function hendleSubmit() {
+function hendleSubmit(event) {
+  event.preventDefault();
   console.log("ok");
 }
