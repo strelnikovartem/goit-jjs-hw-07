6027d1bea89fe346ae75927e15aa895ec80e28c3
Artem Strelnikov <piramida1237@gmail.com>
2025-07-31 22:04:00 +0200

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,14 @@
+const changeColorBtn = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", onChangeColorBtnClick);
+
+function onChangeColorBtnClick(event) {
+  const randomColor = getRandomHexColor();
+  color.textContent = randomColor;
+  document.body.style.backgroundColor = randomColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
