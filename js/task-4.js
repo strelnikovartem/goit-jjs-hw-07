97312c43c33745eca9f606b38175166fbb53d979
Artem Strelnikov <piramida1237@gmail.com>
2025-07-31 08:49:41 +0200

    sdcdxdc

--- js/task-4.js
+++ js/task-4.js
@@ -4,8 +4,11 @@ form.addEventListener("submit", hendleSubmit);
 
 function hendleSubmit(event) {
   event.preventDefault();
-  const email = event.target.elements.email.value;
-  const password = event.target.elements.password.value;
-
+  const email = event.target.elements.email.value.trim();
+  const password = event.target.elements.password.value.trim();
+  if (email === "" && password === "") {
+    alert`All form fields must be filled in`;
+    return;
+  }
   event.target.reset();
 }
